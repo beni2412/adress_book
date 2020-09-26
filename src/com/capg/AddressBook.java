9abdda7153fef46ca728ea78c8d7dcbad1093404
beni2412 <hardaman97@gmail.com>
2020-09-26 15:08:07 +0530

    [Hardaman] Added: Ability to create a contact in address book

---
+++ src/com/capg/AddressBook.java
@@ -0,0 +1,18 @@
+package com.capg;
+import java.util.*;
+import java.io.*;
+
+public class AddressBook {
+ private List<Contact> addressBook = new ArrayList<Contact>();
+ 
+ public void setAddressBook(List<Contact> m) {
+	 this.addressBook= m;
+ }
+ public List<Contact> getAddressBook(){
+	 return this.addressBook;
+ }
+ public void addContact(Contact contactObj) {
+	 addressBook.add(contactObj);
+ }
+ 
+}
--- src/com/capg/Main.java
+++ src/com/capg/Main.java
@@ -1,8 +1,32 @@
 package com.capg;
+import java.util.*;
+import java.io.*;
 
 public class Main {
+	
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		AddressBook ab = new AddressBook();
 		System.out.println("Welcome to Address Book");
+		System.out.println("Enter first name: ");
+		String firstName = sc.nextLine();
+		System.out.println("Enter last name: ");
+		String lastName = sc.nextLine();
+		System.out.println("Enter address: ");
+		String address = sc.nextLine();
+		System.out.println("Enter city: ");
+		String city = sc.nextLine();
+		System.out.println("Enter state: ");
+		String state = sc.nextLine();
+		System.out.println("Enter zip: ");
+		long zip = sc.nextLong();
+		System.out.println("Enter phone number: ");
+		sc.nextLine();
+		String phoneNumber = sc.nextLine();
+		System.out.println("Enter email: ");
+		String email = sc.nextLine();
+		Contact x = new Contact(firstName, lastName, address, city, state, zip, phoneNumber, email);
+		ab.addContact(x);
 	}
 
 }
