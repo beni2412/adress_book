e0b3c286e7f2405e83d6154aced87f9e68041776
beni2412 <hardaman97@gmail.com>
2020-09-27 17:01:59 +0530

    [Hardaman] Added: Ability to edit a contact by name

--- src/com/capg/AddressBook.java
+++ src/com/capg/AddressBook.java
@@ -17,5 +17,18 @@ public class AddressBook {
  public List<Contact> viewContacts() {
 	 return addressBook;
  }
- 
+ public String updateContact(Contact a) {
+	 int y=0;
+	 for(Contact x: addressBook) {
+		 if(x.firstName.equalsIgnoreCase(a.firstName) && x.lastName.equalsIgnoreCase(a.lastName)) {
+		 addressBook.remove(x);
+		 addressBook.add(a);
+		 y++;
+		 }
+	 }
+	 if(y==0)
+		 return "No contact found with this name";
+	 else
+		 return "Contact Updated";
+ }
 }
